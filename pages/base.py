f4efbdf6f659281c8cb597029c8199d9750c2c20
alebusta <122423239+alebusta@users.noreply.github.com>
2025-04-04 19:41:58 -0300

    Update base.py

--- pages/base.py
+++ pages/base.py
@@ -28,7 +28,7 @@ st.markdown("""
 st.markdown("""
 
 <div class="trending-bar">
-        <div class="trending-title"><a href="https://tradewar-monitor.streamlit.app/" target="_blank"> < Volver al inicio  </a></div>
+        <div class="trending-title"><a href="https://tradewar-monitor.streamlit.app/"> < Volver al inicio  </a></div>
         
 </div>
 """, unsafe_allow_html=True)
