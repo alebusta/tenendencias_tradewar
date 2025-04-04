8aef86f40f56dd894278a8d400f84e40804b45ac
alebusta <122423239+alebusta@users.noreply.github.com>
2025-04-04 20:00:25 -0300

    Update base.py

--- pages/base.py
+++ pages/base.py
@@ -24,7 +24,7 @@ st.markdown("""
 """, unsafe_allow_html=True)
 
 if st.button("Ir a Inicio"):
-    st.switch_page("output_dia")
+    st.switch_page("./")
     
 st.page_link("https://tradewar-monitor.streamlit.app/", label="Ir a Inicio", icon="🏠")
 st.markdown("[Volver al inicio](./)")
