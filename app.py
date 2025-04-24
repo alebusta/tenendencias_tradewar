d5efccb78d8a2e05ca138660a40343bede6bfe04
alebusta <122423239+alebusta@users.noreply.github.com>
2025-04-24 15:56:42 -0400

    Update app.py

--- app.py
+++ app.py
@@ -84,13 +84,13 @@ st.markdown(f"""
     <div class="news-grid">
     <!-- Fila 5 -->
     <div class="news-block news-text">
-        <div class="news-title">La mayor víctima de una guerra comercial con China, temen los expertos, podría ser el lugar de Estados Unidos en el mundo</div>
-        <div class="news-date">Publicado el 18 de abril de 2025</div>
-        <div class="news-description">La guerra arancelaria de la administración Trump con China suele verse principalmente desde la perspectiva económica. Pero para muchos economistas, la amenaza más grande es que la apuesta comercial global de Trump podría erosionar la principal fuente de autoridad mundial de Estados Unidos: las relaciones geopolíticas a largo plazo que ha cultivado durante casi un siglo.</div>
-        <a class="news-link" href="https://www.msn.com/en-us/money/markets/the-biggest-casualty-of-a-china-trade-war-americas-place-in-the-world-experts-fear/ar-AA1DbwFI">Ver más →</a>
+        <div class="news-title">La verdadera amenaza tras la guerra comercial de Trump: la ofensiva monetaria de China</div>
+        <div class="news-date">Publicado el 23 de abril de 2025</div>
+        <div class="news-description">China ha trabajado incansablemente para elevar el perfil internacional del RMB. Si bien el progreso ha sido gradual, conlleva riesgos reales para el dominio del dólar estadounidense.</div>
+        <a class="news-link" href="https://thediplomat.com/2025/04/the-real-threat-behind-trumps-trade-war-chinas-currency-offensive/">Ver más →</a>
         </div>
     <div class="news-block">
-        <img class="news-image" src="https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AA1DbFnA.img?w=768&h=612&m=6&x=222&y=247&s=600&d=158" />
+        <img class="news-image" src="https://thediplomat.com/wp-content/uploads/2021/04/sizes/medium/thediplomat-2021-04-19-6.jpg" />
     </div>
     <!-- Fila 2 -->
     <div class="news-block">
