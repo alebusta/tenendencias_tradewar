6e303a83e2dec1df032a90740a8a07f9a56dd6ba
alebusta <122423239+alebusta@users.noreply.github.com>
2025-04-28 10:45:16 -0400

    Update resumen.py

--- pages/resumen.py
+++ pages/resumen.py
@@ -28,7 +28,7 @@ st.set_page_config(
 
 
 # Cargar CSS desde un archivo externo
-with open("front/style.css", "r") as f:
+with open("style.css", "r") as f:
     css = f.read()
     st.markdown(f"<style>{css}</style>", unsafe_allow_html=True)
 
