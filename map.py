e1a9643108cb4f2fe55538fc60a5905379d40836
alebusta <122423239+alebusta@users.noreply.github.com>
2025-05-06 17:42:18 -0400

    Update map.py

--- map.py
+++ map.py
@@ -10,7 +10,13 @@ import streamlit as st
 
 def impact_map():
 
-    df = pd.read_csv('database.csv')
+    file_id = '1C5w6w3u-pFKv3-l7iLAGhphyO9nn4Xmc'
+    url = f'https://drive.google.com/uc?id={file_id}'
+
+    df = pd.read_csv(url)
+   
+
+    #df = pd.read_csv('database.csv')
 
     # Crear un DataFrame con los recuentos de etiquetas países
     df['country'] = df['country'].apply(ast.literal_eval)
