f90333e6ee058390ed12810b6c60d5a5aae0f462
alebusta <122423239+alebusta@users.noreply.github.com>
2025-05-06 18:25:51 -0400

    Update app.py

--- app.py
+++ app.py
@@ -119,11 +119,11 @@ st.markdown(f"""
         <a class="news-link" href="https://www.rfi.fr/es/m%C3%A1s-noticias/20250415-eeuu-impondr%C3%A1-casi-un-21-de-aranceles-a-la-mayor%C3%ADa-de-los-tomates-mexicanos">Ver más →</a>
         </div>   
     <div class="news-block">
-        <img class="news-image" src="https://s.rfi.fr/media/display/043bf16e-19e6-11f0-b4cb-005056bf30b7/w:246/p:16x9/15248f8203b511812c4b2d6b3fcd5460be434102.webp 246w" />
+        <img class="news-image" src="https://s.rfi.fr/media/display/043bf16e-19e6-11f0-b4cb-005056bf30b7/w:980/p:16x9/15248f8203b511812c4b2d6b3fcd5460be434102.webp" />
     </div>    
     <!-- Fila 1 -->
     <div class="news-block">
-        <img class="news-image" src="https://s.rfi.fr/media/display/043bf16e-19e6-11f0-b4cb-005056bf30b7/w:980/p:16x9/15248f8203b511812c4b2d6b3fcd5460be434102.webp" />
+        <img class="news-image" src="https://static01.nyt.com/images/2025/05/05/multimedia/05int-elections-trump-02-pjhl/05int-elections-trump-02-pjhl-superJumbo.jpg?quality=75&auto=webp" />
     </div>
     <div class="news-block news-text">  
         <div class="news-title">Una nueva tendencia en las elecciones mundiales: el impulso anti-Trump</div>
